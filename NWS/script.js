0fe1a07a6fffbe591209e28c82bb62f1e91659b1
InfoPoliticsStudio <infi.dopa.moi.my@gmail.com>
2024-12-25 10:41:15 +0700

    Update script.js

--- NWS/script.js
+++ NWS/script.js
@@ -2,3 +2,16 @@
 document.getElementById("explore-btn").addEventListener("click", function() {
   document.querySelector("#about").scrollIntoView({ behavior: 'smooth' });
 });
+
+// Toggle sidebar
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+
+menuToggle.addEventListener('click', function() {
+  sidebar.classList.toggle('open');
+});
+
+// Scroll to section on button click
+document.getElementById("explore-btn").addEventListener("click", function() {
+  document.querySelector("#about").scrollIntoView({ behavior: 'smooth' });
+});
