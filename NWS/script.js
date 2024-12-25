d80b693de6dfbefd3d767d7a69c980ee082e995a
InfoPoliticsStudio <infi.dopa.moi.my@gmail.com>
2024-12-25 10:27:49 +0700

    Create script.js

---
+++ NWS/script.js
@@ -0,0 +1,4 @@
+// Scroll to section on button click
+document.getElementById("explore-btn").addEventListener("click", function() {
+  document.querySelector("#about").scrollIntoView({ behavior: 'smooth' });
+});
