5e264c06a55162892dd4ed22a2c237d390b1988d
InfoPoliticsStudio <infi.dopa.moi.my@gmail.com>
2025-03-16 12:26:42 +0700

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
+    const icon = menuToggle.querySelector("i");
 
     menuToggle.addEventListener("click", function () {
         navLinks.classList.toggle("nav-active");
+        if (navLinks.classList.contains("nav-active")) {
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-times");
+        } else {
+            icon.classList.remove("fa-times");
+            icon.classList.add("fa-bars");
+        }
     });
 });
