94900eaa830decdfa224102339e0dcbf7124bbcb
InfoPoliticsStudio <infi.dopa.moi.my@gmail.com>
2025-03-16 12:33:28 +0700

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,16 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector("#menu-toggle");
+    const menuToggle = document.querySelector(".fas");
     const navLinks = document.querySelector(".nav-links");
     const icon = menuToggle.querySelector("i");
 
     menuToggle.addEventListener("click", function () {
         navLinks.classList.toggle("nav-active");
-        if (navLinks.classList.contains("nav-active")) {
-            icon.classList.remove("fa-bars");
-            icon.classList.add("fa-times");
-        } else {
-            icon.classList.remove("fa-times");
-            icon.classList.add("fa-bars");
-        }
+        navLinks.classList.toggle("fa-times");
     });
 });
