e6c7f0858f1fd35f29fa4086387de56dc3057ced
InfoPoliticsStudio <infi.dopa.moi.my@gmail.com>
2025-03-20 14:59:45 +0700

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,20 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
-    const icon = menuToggle.querySelector("i");
-
-    if (!menuToggle || !navLinks || !icon) {
-        console.error("Element not found!");
-        return;
-    }
-
-    menuToggle.addEventListener("click", function () {
-        navLinks.classList.toggle("nav-active");
-        
-        icon.classList.toggle("fa-bars");
-        icon.classList.toggle("fa-times");
-
-        console.log("Icon class:", icon.classList);
-    });
-});
+document.addEventListener("DOMContentLoaded", function () {const menuToggle = document.querySelector(".menu-toggle");const navLinks = document.querySelector(".nav-links");const icon = menuToggle.querySelector("i");if (!menuToggle || !navLinks || !icon) {console.error("Element not found!");return;}menuToggle.addEventListener("click", function () {navLinks.classList.toggle("nav-active");     icon.classList.toggle("fa-bars");icon.classList.toggle("fa-times");console.log("Icon class:", icon.classList);});});
 document.querySelector('.btn').addEventListener('click', function() {if (this.hasAttribute('data-log-in')) {window.location.href = '/public/login';}});
+window.addEventListener("load", () => {const loader = document.querySelector(".css-1ltj605");document.body.style.overflowY = "hidden";setTimeout(() => {loader.classList.add("css-1ltj605-hidden");loader.addEventListener("transitionend", () => {document.body.removeChild(loader);document.body.style.overflowY = "";});}, 3000);});
